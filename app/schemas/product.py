eb4762641d20cdca8698350d65b96d2b48508538
rmenai <rami.menai@outlook.com>
2021-10-04 18:41:51 +0300

    Fix product.py docstrings conventions

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -6,21 +6,31 @@ from pydantic import BaseModel
 
 
 class ProductBase(BaseModel):
+    """The base product model."""
+
     id: Optional[int]
     name: Optional[str]
     price: Optional[float]
 
 
 class ProductCreate(BaseModel):
+    """POST request product model."""
+
     name: str
     price: float
 
 
 class ProductUpdate(BaseModel):
+    """PUT request product model."""
+
     name: Optional[str]
     price: Optional[float]
 
 
 class ProductResponse(ProductBase):
+    """GET request product model."""
+
     class Config:
+        """The Pydantic configuration class."""
+
         orm_mode = True
