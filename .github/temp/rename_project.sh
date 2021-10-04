8f74aa10528103e1b0ee8e8c5357295e5e9af9fe
rmenai <rami.menai@outlook.com>
2021-10-04 21:04:51 +0300

    Fix renaming script

--- .github/temp/rename_project.sh
+++ .github/temp/rename_project.sh
@@ -22,12 +22,12 @@ title="$(TitleCaseConverter "$title")"
 repo="$author/$name"
 
 # Change backend pyproject.toml
-sed -i "s/website-structure/$name/g" backend/pyproject.toml
-sed -i "s/My complete websites projects structure/$description/g" backend/pyproject.toml # Remove description
+sed -i "s/fastapi/$name/g" backend/pyproject.toml
+sed -i "s/A FastAPI template/$description/g" backend/pyproject.toml # Remove description
 sed -i "s/rmenai <rami.menai@outlook.com>/$author/g" backend/pyproject.toml # Replace authors
 
 # Change LICENSE
-sed -i "s/website-structure/$name/g" LICENSE
+sed -i "s/fastapi/$name/g" LICENSE
 
 # Change SECURITY.md
 sed -i "s/rmenai/$author/g" SECURITY.md
