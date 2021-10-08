b8b351e50e4dde19dd8923034e09f303fb333133
Rami Menai <rami.menai@outlook.com>
2021-10-08 19:23:51 +0300

    Rename License owner

--- .github/temp/rename_project.sh
+++ .github/temp/rename_project.sh
@@ -27,7 +27,7 @@ sed -i "s/My FastAPI template/$description/g" pyproject.toml # Remove descriptio
 sed -i "s/rmenai <rami.menai@outlook.com>/$author/g" pyproject.toml # Replace authors
 
 # Change LICENSE
-sed -i "s/fastapi/$name/g" LICENSE
+sed -i "s|rmenai-blueprints/fastapi|$name|g" LICENSE # Separator is |
 
 # Change SECURITY.md
 sed -i "s/rmenai/$author/g" SECURITY.md
