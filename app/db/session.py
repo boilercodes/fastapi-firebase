8ac38b5a57e62c8ef836a2273bb33e42d4a1d379
rmenai <rami.menai@outlook.com>
2021-11-07 22:04:05 +0300

    Use SQLite file database if pg not working

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists
 
 from app.core import settings
+from app.db import Base
 
-engine = create_engine(settings.pg_dns, pool_pre_ping=True)
+# Use sqlite if postgres doesn't work.
+if database_exists(settings.pg_dns):
+    engine = create_engine(settings.pg_dns)
+else:
+    engine = create_engine("sqlite:///db.sqlite3")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Initialise all tables and columns.
+Base.metadata.create_all(engine, checkfirst=True)
