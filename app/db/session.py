a8658d024b3b5e2881e2ee33d74bab849a8c3c74
rmenai <rami.menai@outlook.com>
2021-11-08 23:12:04 +0300

    Add Session object

--- app/db/session.py
+++ app/db/session.py
@@ -1,3 +1,8 @@
+import logging
+import multiprocessing
+import sys
+import time
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists
@@ -5,13 +10,55 @@ from sqlalchemy_utils import database_exists
 from app.core import settings
 from app.db import Base
 
-# Use sqlite if postgres doesn't work.
-if database_exists(settings.pg_dns):
-    engine = create_engine(settings.pg_dns)
+log = logging.getLogger(__name__)
+
+
+class Session(sessionmaker):
+    """Represents a database session."""
+
+    def __init__(self, use_sqlite: bool = False, **kw):
+        # Add a background task to try to connect to the database.
+        super().__init__(**kw)
+        self.engine = None
+
+        if use_sqlite:
+            # Manually call the error callback to use sqlite.
+            self.error_callback(BaseException("Using sqlite as stated."))
+        else:
+            pool = multiprocessing.Pool()
+            pool.apply_async(self.get_engine, (3, 1), callback=self.callback,
+                             error_callback=self.error_callback)
+
+    def callback(self, pg_dns: str) -> None:
+        """Execute when the pool is completed."""
+        self.engine = create_engine(pg_dns)
+        self.kw["bind"] = self.engine
+
+        Base.metadata.create_all(self.engine, checkfirst=True)
+
+    def error_callback(self, error: BaseException) -> None:
+        """Execute when the pool throws an error."""
+        log.info(error)
+
+        # Use sqlite if the connection to the database failed.
+        self.engine = create_engine("sqlite:///db.sqlite3")
+        self.kw["bind"] = self.engine
+
+        Base.metadata.create_all(self.engine, checkfirst=True)
+
+    @staticmethod
+    def get_engine(timeout: int, delay: int) -> str:
+        """Get an engine depending on the settings db."""
+        for _ in range(timeout):
+            if database_exists(settings.pg_dns):
+                return settings.pg_dns
+            time.sleep(delay)
+
+        raise ConnectionError("Could not connect to the database, using sqlite.")
+
+
+if "pytest" in sys.modules:
+    # Use sqlite if running tests.
+    SessionLocal = Session(use_sqlite=True, autocommit=False, autoflush=False)
 else:
-    engine = create_engine("sqlite:///db.sqlite3")
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Initialise all tables and columns.
-Base.metadata.create_all(engine, checkfirst=True)
+    SessionLocal = Session(autocommit=False, autoflush=False)
