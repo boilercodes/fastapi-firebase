88cf95f763826cfd33200b1cfe730c53c126ac21
rmenai <rami.menai@outlook.com>
2021-11-08 23:11:35 +0300

    Add custom logging

---
+++ app/__init__.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import logging.handlers
+import os
+from pathlib import Path
+
+from uvicorn.config import LOGGING_CONFIG
+
+from app.core import settings
+
+# Set up file logging.
+log_dir = Path("app/logs")
+log_file = log_dir / f"{datetime.now().strftime('%d-%m-%Y')}.log"
+os.makedirs(log_dir, exist_ok=True)
+
+# File handler rotates logs every 5 MB.
+file_handler = logging.handlers.RotatingFileHandler(
+    log_file, maxBytes=5 * (2 ** 20), backupCount=10, encoding="utf-8",
+)
+file_handler.setLevel(logging.DEBUG)
+
+# Console handler prints to terminal.
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+
+# Format configuration.
+fmt = "%(asctime)s - %(name)s %(levelname)s: %(message)s"
+datefmt = "%H:%M:%S"
+
+# Add colors for logging if available.
+try:
+    from colorlog import ColoredFormatter
+
+    console_handler.setFormatter(
+        ColoredFormatter(fmt=f"%(log_color)s{fmt}", datefmt=datefmt))
+except ModuleNotFoundError:
+    pass
+
+# Remove old loggers, if any.
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+
+# Setup new logging configuration.
+logging.basicConfig(
+    format=fmt,
+    datefmt=datefmt,
+    level=logging.DEBUG,
+    handlers=[console_handler, file_handler]
+)
+
+# Configure uvicorn loggers.
+LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s - %(name)s %(levelname)s: %(message)s"
+LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%H:%M:%S"
+LOGGING_CONFIG["loggers"]["uvicorn"].pop("handlers")
+
+log = logging.getLogger(__name__)
+
+log.debug("Logging initialization complete")
